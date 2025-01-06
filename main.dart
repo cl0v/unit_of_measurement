233bf02561681a2080fac127572f32941945b1bc
Marcelo Viana <marcelofv12@hotmail.com>
2025-01-06 07:59:51 -0300

    3 basic units are now Units instead of kind

--- main.dart
+++ main.dart
@@ -1,12 +1,12 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class Unit {
   final double magnitude;
-  final Kind kind;
+  final List<Kind> kind;
 
   Unit(
     this.magnitude,
     this.kind,
-  );
+  ) : assert(kind.length <= 3);
 
   @override
   toString() => '$magnitude $kind';
@@ -16,9 +16,48 @@ class Unit {
       return Unit(magnitude * other, kind);
     }
     if (other is Unit) {
-      return Unit(magnitude * other.magnitude, kind + other.kind);
+      return Unit(
+        magnitude * other.magnitude,
+        kind + other.kind,
+      );
     }
   }
+}
+
+class Meter extends Unit {
+  Meter(
+    double magnitude, [
+    int dimension = 1,
+  ]) : super(
+          magnitude,
+          [
+            Kind('m', dimension),
+          ],
+        );
+}
+
+class Second extends Unit {
+  Second(
+    double magnitude, [
+    int dimension = 1,
+  ]) : super(
+          magnitude,
+          [
+            Kind('s', dimension),
+          ],
+        );
+}
+
+class Gram extends Unit {
+  Gram(
+    double magnitude, [
+    int dimension = 1,
+  ]) : super(
+          magnitude,
+          [
+            Kind('g', dimension),
+          ],
+        );
 }
 
 class Kind {
@@ -30,33 +69,49 @@ class Kind {
     this.dimension,
   );
 
-  operator +(Kind other) {
-    if (symbol != other.symbol) {
-      throw ArgumentError('Symbols must be the same');
-    }
-    return Kind(symbol, dimension + other.dimension);
-  }
+  // operator +(Kind other) {
+  //   if (symbol != other.symbol) {
+  //     throw ArgumentError('Symbols must be the same');
+  //   }
+  //   return Kind(
+  //     symbol,
+  //     dimension + other.dimension,
+  //   );
+  // }
+
+  // List<Kind> operator -(List<Kind> other) {
+  //   if (symbol != other.symbol) {
+  //     throw ArgumentError('Symbols must be the same');
+  //   }
+  //   return [
+  //     Kind(
+  //       symbol,
+  //       dimension - other.dimension,
+  //     )
+  //   ];
+  // }
+
+  // List<Kind> operator +(List<Kind> other) {
+  //   String symbol = this.symbol;
+  //   if (other.symbol != symbol) {
+  //     return Kind(
+  //       "($symbol . ${other.symbol})",
+  //       dimension * other.dimension,
+  //     );
+  //   }
+  //   return Kind(
+  //     symbol,
+  //     dimension * other.dimension,
+  //   );
+  // }
 
   @override
   toString() => '$symbol$dimension';
 }
 
-class Meter extends Kind {
-  Meter(int dimension) : super('m', dimension);
-}
-
-class Second extends Kind {
-  Second(int dimension) : super('s', dimension);
-}
-
-class Kilogram extends Kind {
-  Kilogram(int dimension) : super('kg', dimension);
-}
-
-
 /// Usage example:
 void main() {
-  final meter = Unit(10, Meter(1));
+  final meter = Meter(10, 1);
 
-  print(meter * Unit(1, Kind('m', 1)));
+  print(meter * Meter(1));
 }
